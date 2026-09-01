c0c496269c244b726b9d80eb95314b20c4b6fc4c
merveillematondo2027-design <merveillematondo2027@gmail.com>
2026-09-01 20:21:10 +0100

    chore: rebrand project to Market-Cash
    Update application name, metadata, and configuration to match the Market-Cash branding. Remove obsolete maintenance and utility scripts.

--- test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/pages/client/Cards.tsx', 'utf8');
-const lines = content.split('\n');
-
-let openTags = [];
-// This is not a full XML parser, but I can just run tsc.
--- update_card_service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-# We'll use node to write the methods into cardService.ts
