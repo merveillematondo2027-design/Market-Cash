59f99d862fc3898aa0db03cbca3b28e129ed6d44
merveillematondo2027-design <merveillematondo2027@gmail.com>
2026-09-10 15:39:04 +0100

    feat(payment-control): add SMS responder service placeholder

---
+++ android-admin/app/src/main/java/com/marketcash/admin/RespondViaMessageService.kt
@@ -0,0 +1,9 @@
+package com.marketcash.admin
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class RespondViaMessageService : Service() {
+    override fun onBind(intent: Intent?): IBinder? = null
+}
